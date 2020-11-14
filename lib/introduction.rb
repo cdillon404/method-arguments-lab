d0dd7776741a396f67c838a6799ff2770da121a7
cdillon404 <chavin@gmail.com>
2020-11-14 11:24:34 -0700

    commit tree be652bb15f8960bacd8230394e9e06c0505f69bb

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction (name)
+    puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language (name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def greeting (name="Ruby programmer", language="Ruby")
+    puts "Hellow, #{name}. We heard you are a great #{language} programmer."
+end 
